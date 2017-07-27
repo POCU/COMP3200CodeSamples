16c8e0ae202d5c2287b53be9f91ed9c4b0b53504
Pope Kim <popekim@gmail.com>
2017-07-27 11:13:43 -0700

    added hello world example

---
+++ 00/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include "HelloWorld.h"
+
+#include <iostream>
+
+void SayHello()
+{
+	std::cout << "Hello World" << std::endl;
+}
---
+++ 00/Main.cpp
@@ -0,0 +1,6 @@
+#include "HelloWorld.h"
+
+int main()
+{
+	SayHello();
+}
