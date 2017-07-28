bdca1f9a275ed8e82808ca14350ebd65db67aee8
ExtremeSkepticist <hyouk0902@gmail.com>
2017-07-28 17:00:44 -0600

    Week 1 1. Namespace

--- 01/HelloPope.cpp
+++ 01/HelloPope.cpp
@@ -1,1 +1,8 @@
+#include <iostream>
 #include "HelloPope.h"
+using namespace std;
+
+void hello::SayHello()
+{
+	cout << "Hello Pope." << endl;
+}
--- 01/HiPope.cpp
+++ 01/HiPope.cpp
@@ -1,1 +1,7 @@
+#include <iostream>
 #include "HiPope.h"
+
+void hi::SayHello()
+{
+	std::cout << "Hi Pope." << endl;
+}
