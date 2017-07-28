fbea16bf072f1b1441442261cbf1abd0fd76bae3
HH <hyouk0902@gmail.com>
2017-07-28 17:38:47 -0600

    Update PrintAll.cpp

--- 01/PrintAll.cpp
+++ 01/PrintAll.cpp
@@ -2,15 +2,18 @@
 #include "PrintAll.h"
 using namespace std;
 
-void samples::PrintEverything()
+namespace samples
 {
-	int integer = 10;
-	float decimal = 1.5f;
-	char letter = 'A';
-	char string[] = "Hello, world!";
+	void PrintEverything()
+	{
+		int integer = 10;
+		float decimal = 1.5f;
+		char letter = 'A';
+		char string[] = "Hello, world!";
 
-	cout << integer << endl;
-	cout << decimal << endl;
-	cout << letter << endl;
-	cout << string << endl;
+		cout << integer << endl;
+		cout << decimal << endl;
+		cout << letter << endl;
+		cout << string << endl;
+	}
 }
