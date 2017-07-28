d21841212a30773ab3f0bdedba1051271bbbaaac
ExtremeSkepticist <hyouk0902@gmail.com>
2017-07-28 16:30:14 -0600

    Week 1 2. Print everything

--- 01/PrintAll.cpp
+++ 01/PrintAll.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 #include "PrintAll.h"
+using namespace std;
+
+void samples::PrintEverything()
+{
+	int integer = 10;
+	float decimal = 1.5f;
+	char letter = 'A';
+	char string[] = "Hello, world!";
+
+	cout << integer << endl;
+	cout << decimal << endl;
+	cout << letter << endl;
+	cout << string << endl;
+
+	return 0;
+}
