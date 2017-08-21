677adf5e393498c4bc3435357e74928c39627250
Pope Kim <popekim@gmail.com>
2017-08-20 20:00:31 -0700

    fixed compiler warning

--- 02/VectorExample1.cpp
+++ 02/VectorExample1.cpp
@@ -12,7 +12,7 @@ namespace samples
 		cout << "|       Vector Example 1       |" << endl;
 		cout << "+------------------------------+" << endl;
 
-		Vector1 v1();
+		Vector1 v1;
 		Vector1 v2(10, 55);
 	}
 }
