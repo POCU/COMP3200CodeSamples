902cda07dadd117cd54bb795b92e6c7a91b75b5c
Pope Kim <popekim@gmail.com>
2017-11-02 20:20:59 -0700

    spacing

--- 08/FindFunctionExample.cpp
+++ 08/FindFunctionExample.cpp
@@ -10,8 +10,8 @@ namespace samples
 	void FindFunctionExample()
 	{
 		const int ARRAY_SIZE = 10;
-		int arr[ARRAY_SIZE] = { 10,20,13,52,32,67,89,15,46,3 };
-		
+		int arr[ARRAY_SIZE] = { 10, 20, 13, 52, 32, 67, 89, 15, 46,3 };
+
 		const int* ptr = Algorithm::Find(arr, arr + ARRAY_SIZE, 67);
 		cout << *ptr;
 	}
