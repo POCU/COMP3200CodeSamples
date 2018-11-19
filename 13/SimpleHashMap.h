03ab73807319279fd1f3877145f506143a888277
monkeywithbanana <>
2018-11-18 18:57:29 -0800

    bug

--- 13/SimpleHashMap.h
+++ 13/SimpleHashMap.h
@@ -19,7 +19,7 @@ namespace samples
 		void Print() const;
 
 	private:
-		//constexpr int hashFunctionWithCompileError(const char* s);
+		//static constexpr int hashFunctionWithCompileError(const char* s);
 		static constexpr int hashFunction(const char* s, size_t length);
 
 		static constexpr int MAX_SIZE = 10;
