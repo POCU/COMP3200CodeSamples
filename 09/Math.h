69f0bfa7ffeb3eacb13f23cce905bcf96afab95a
Pope Kim <pope.kim@pocu.academy>
2026-01-04 15:52:21 -0800

    fixed math functions to be inline instead of static

--- 09/Math.h
+++ 09/Math.h
@@ -5,31 +5,31 @@ namespace samples
 	namespace Math
 	{
 		template <typename T>
-		static T Add(T value1, T value2)
+		inline T Add(T value1, T value2)
 		{
 			return value1 + value2;
 		}
 		
 		template <typename T>
-		static T Divide(T value1, T value2)
+		inline T Divide(T value1, T value2)
 		{
 			return value1 / value2;
 		}
 		
 		template <typename T>
-		static T Max(T value1, T value2)
+		inline T Max(T value1, T value2)
 		{
 			return value1 < value2 ? value2 : value1;
 		}
 		
 		template <typename T>
-		static T Min(T value1, T value2)
+		inline T Min(T value1, T value2)
 		{
 			return value1 < value2 ? value1 : value2;
 		}
 		
 		template <typename T>
-		static T Multiply(T value1, T value2)
+		inline T Multiply(T value1, T value2)
 		{
 			return value1 * value2;
 		}
